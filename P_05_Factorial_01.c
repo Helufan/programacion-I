971347a5efcc7d166114048eba58a1ecb9376156
Helufan <33005907+Helufan@users.noreply.github.com>
2017-10-27 10:38:53 -0500

    Update P_05_Factorial_01.c

--- P_05_Factorial_01.c
+++ P_05_Factorial_01.c
@@ -1,17 +1,42 @@
-#include <stdio.h>
-#include <math.io>
-
-/*
-Objetivo: Calcular el factorial de un número entero positivo
-*/
-
-int main()
-
+#include<stdio.h>
+int factorial(float x)
+{
+	float n, factor;
+	factor=1;
+	for(n=x;n>1;n--)
+	{
+		factor=factor*n;
+	}
+	printf("El valor del factorial es:%f",factor);
+	return factor;
+}
+int caratula()
 {
 	
-	
-	
-	
+	printf("\n\t\tUniversidad Nacional Autonoma de Mexico\n");
+	printf("\tPrograma que calcula el factorial de un n%cmero entero positivo\n",130);
+	printf("\n\n\tIntegrantes:\n\tGalvan Lugo Alberto Hazel\n\tLuis %cngel Hern%Cndez Flores\n",181,160);
+	printf("\tMonreal Tovar Victor Armando\n\tSantiago Reyes Donovan Ariel\n\tRamirez Chavez Monica\n\n\n");
+	system("pause > Hola");
+	system("cls");
+}
+int programa()
+{
+	float x;
+	printf("Ingrese un n%cmero entero positivo:",160);
+	scanf("%f",&x);
+	factorial(x);
+
+}
+int main()
+{ 
+	caratula();
+	int respuesta;
+	do{	system("cls");
+		programa();
+		printf("\nDesea evaluar un nuevo numero? Si=1 No=0:\t");
+		scanf("%f",&respuesta);
 		
-	return 0;
+
+	}while(respuesta==1);
 }
